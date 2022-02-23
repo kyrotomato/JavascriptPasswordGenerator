76a888dbbd8a2304180ebcacd7d467dfa78fc01f
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-02-23 11:25:11 -0600

    changed length and charpool variables to proper values

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,8 +4,8 @@ var lowerChars = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","
 var upperChars = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ];
 var symbols = [ "!","@","#","$","%","^","&","*","(",")","-","_","=","+","'",";",":","<",",",">",".","/","?","*" ];
 var nums = [ "1","2","3","4","5","6","7","8","9","0" ];
-var charLength = "0";
-var charpool = "";
+var charLength = 0;
+var charpool = [];
 
 
 function promptAnswers(){
